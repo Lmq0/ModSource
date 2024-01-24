29e16ff47972c8dcdf600e13ccd3bf1b7e575160
Lmq0 <157557481+Lmq0@users.noreply.github.com>
2024-01-24 11:22:06 +0800

    Add files via upload

---
+++ MyPlayer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.GameContent;
+using Terraria.GameContent.Drawing;
+using Terraria.DataStructures;
+
+using Terraria.Localization;
+using test.Projectiles;
+using test.Projectiles.HolyRay;
+using test.Buffs;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using Terraria.Graphics.Effects;
+
+namespace test
+{
+    public class MyPlayer:ModPlayer
+    {
+
+    
+       
+      
+
+
+
+
+
+    }
+}
---
+++ test.cs
@@ -0,0 +1,22 @@
+using Terraria.ModLoader;
+using Terraria.ID;
+using test.NPCs.GoddnessProvidence;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria.DataStructures;
+using test.Projectiles;
+
+namespace test
+{
+	public class test : Mod
+	{
+        public override void Load()
+        {
+
+            base.Load();
+        }
+    }
+}
